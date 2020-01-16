767060068e466762e65002659ac668109b5e6b19
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-01-16 21:36:21 +0100

    Changed winnerList to scoreList

--- Scoreboard.js
+++ Scoreboard.js
@@ -1,20 +1,20 @@
 //Info om vem som vann och antal gissningar
 
-function showWinner(score, player, guesses) /*score = list of scores, player = player score, guesses = number of guesses */ {
-    if (player === Math.max(winnerlist)) {
+function showWinner(scoreList, player, guesses) /*scoreList = list of scoreList, player = player scoreList, guesses = number of guesses */ {
+    if (player === Math.max(scoreList)) {
         document.querySelector("resultText").innerHTML = ("Hooray! You won! After " + guesses)+  " guesses!";
-        document.querySelector("score").innerHTML = (player + score);
-    } else if (bot1 === Math.max(winnerlist)) {
+        document.querySelector("scoreList").innerHTML = (player + scoreList);
+    } else if (bot1 === Math.max(scoreList)) {
         document.querySelector("resultText").innerHTML = (bot1 + " " + "Won! After " + guesses+  " guesses!");
-        document.querySelector("loserText").innerHTML = (player + " " + "You lost :(\n Your total score was:\n" + score);
-        document.querySelector("score").innerHTML = (player + score);
-    }  else if (bot2 === Math.max(winnerlist)) {
+        document.querySelector("loserText").innerHTML = (player + " " + "You lost :(\n Your total scoreList was:\n" + scoreList);
+        document.querySelector("scoreList").innerHTML = (player + scoreList);
+    }  else if (bot2 === Math.max(scoreList)) {
         document.querySelector("resultText").innerHTML = (bot2 + " " + "Won! After " + guesses+  " guesses!");
-        document.querySelector("loserText").innerHTML = (player + " " + "You lost :(\n Your total score was:\n" + score);
-        document.querySelector("score").innerHTML = (player + score);
-    }  else if (bot3 === Math.max(winnerlist)) {
+        document.querySelector("loserText").innerHTML = (player + " " + "You lost :(\n Your total scoreList was:\n" + scoreList);
+        document.querySelector("scoreList").innerHTML = (player + scoreList);
+    }  else if (bot3 === Math.max(scoreList)) {
         document.querySelector("resultText").innerHTML = (bot3 + " " + "Won! After " + guesses +  " guesses!");
-        document.querySelector("loserText").innerHTML = (player + " " + "You lost :(\n Your total score was:\n" + score);
-        document.querySelector("score").innerHTML = (player + score);
+        document.querySelector("loserText").innerHTML = (player + " " + "You lost :(\n Your total scoreList was:\n" + scoreList);
+        document.querySelector("scoreList").innerHTML = (player + scoreList);
     }
 }
