2f8dd7a85962abd8bf2afa072f38073d1d3644bf
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-01-27 09:30:57 +0100

    comitts
    commits

--- settings.js
+++ settings.js
@@ -1,30 +1,47 @@
-// document.querySelector(".set-user-settings").addEventListener("click", checkUserSettings);
-
-//document.querySelector("#see-opponents-guesses").addEventListener("click", seeOpponentsGuesses);
-
-function seeOpponentsGuesses(){
-    let settingsSeeOpponentsGuesses = true;
-    document.querySelector(".see-opponents-guesses").innerHTML = "<p>show opponents guesses: off<p>";
-}
+document.querySelector(".set-user-settings").addEventListener("click", checkUserSettings);
+document.querySelector(".set-user-settings").addEventListener("click", checkUserSettings);
 
 function checkUserSettings(){
-    settingMaxNumberBox = document.querySelector(".settings-input")["0"];
-    if (isNaN(parseInt(settingMaxNumberBox.value))){
-        settingMaxNumberBox.value = "Error";
-    } else {
-        maxNumber = parseInt(settingMaxNumberBox.value);
-        settingMaxNumberBox.value = "Max now: " + maxNumber;
-        localStorage.setItem('maxNumber', JSON.stringify(maxNumber));
+    let settingMaxNumberBox = document.querySelector(".set-user-maxNumber");
+    let timePressureButton = document.getElementById("timePressureButton");
+    let showOpponentsButton = document.getElementById("showOpponentsButton");
+    let settings = {
+        settingShowGuessesOn: true,
+        settingMaxNumber: 20,
+        settingTimePressureOn: true
     }
 
+    if (isNaN(parseInt(settingMaxNumberBox.value))){
+        //If a non-number is put in, the setting-object resets it, so tempSettings gets settingMaxNumber from local storage
+        settingMaxNumberBox.value = "Error";
+        if ("settings" in localStorage){
+            let tempSettings = JSON.parse(localStorage.getItem("settings"));
+            settings.settingMaxNumber = tempSettings.settingMaxNumber;
+        } else {
+            settingMaxNumberBox.placeholder = "Max Range: 20";
+        }
+    } else {
+        settings.settingMaxNumber = parseInt(settingMaxNumberBox.value);
+        settingMaxNumberBox.value = "Max now: " + settings.settingMaxNumber;
+    } 
+
+    if (timePressureButton.classList == "buttonClicked"){
+        settings.settingTimePressureOn = false;
+    }
+
+    if (showOpponentsButton.classList == "buttonClicked"){
+        settings.settingShowGuessesOn = false;
+    }
+    localStorage.setItem('settings', JSON.stringify(settings));
 }
 
 //change text in settings
 function setSettingValues(){
-    let settingMaxNumberBox = document.querySelector(".settings-input")["0"];
-    if (localStorage.getItem("maxNumber") === null){
-        settingMaxNumberBox.placeholder = "Max Range: 20";
+    let settingMaxNumberBox = document.querySelector(".set-user-maxNumber");
+    let settings = JSON.parse(localStorage.getItem("settings"));
+    if ("settings" in localStorage){
+        settingMaxNumberBox.placeholder = "Max Range: " + settings.settingMaxNumber;
     } else {
-        settingMaxNumberBox.placeholder = "Max Range: " + localStorage.getItem("maxNumber");
+        settingMaxNumberBox.placeholder = "Max Range: 20";
     }
 }
