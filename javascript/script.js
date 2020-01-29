2c95953bb984f48d93992963bc8fe02212d9ca29
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-01-29 16:04:28 +0100

    changed some text

--- javascript/script.js
+++ javascript/script.js
@@ -410,7 +410,7 @@ function calculateScore(secretNumber, guess, player, maxNumber, minNumber) {
  */
 function showCorrectStartbuttonText(){
     if(!isThreeBotsSelected){
-        document.querySelector('#submitUsername').textContent = "That's not 3 bots...";      
+        document.querySelector('#submitUsername').textContent = "That's not 3 opponents...";      
     }
     else{
         document.querySelector('#submitUsername').textContent = "Start playing!";
